31a0e8c31b30a577181d17f84b9625d09ba51f3c
Ned Read <needarb@gmail.com>
2014-03-27 19:54:29 -0500

    Parts

--- src/edu/macalester/comp124/hw5/CharacterCreationGUI.java
+++ src/edu/macalester/comp124/hw5/CharacterCreationGUI.java
@@ -2,9 +2,12 @@ package edu.macalester.comp124.hw5;
 
 import javax.print.DocFlavor;
 import javax.swing.*;
+import java.applet.Applet;
+import java.applet.AudioClip;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -16,9 +19,12 @@ public class CharacterCreationGUI extends JFrame implements ActionListener
 
     public static void main(String[] args)
     {
-        CharacterCreationGUI g = new CharacterCreationGUI();
+     //   playSound("test.wav");
+       CharacterCreationGUI g = new CharacterCreationGUI();
         g.askNextQuestion();
     }
+
+    public static final String[] STAT_NAMES = {"Health","Attack","Defense","Speed"};
 
     public static final String[] STANDARD_QUESTIONS = {"gender", "height", "weight"};
     public static final String[] FAVORITE_QUESTIONS = {"food", "boy band", "method of dying", "mathematical symbol", "method of transportation", "type of music"};
@@ -42,33 +48,63 @@ public class CharacterCreationGUI extends JFrame implements ActionListener
 
     }
 
-    private JLabel questionLabel;
-    private JPanel answerPanel;
+
     private JPanel questionPanel;
+    private JPanel statsPanel;
+    private JPanel answerPanel;
+
+    private JLabel questionLabel;
+    private JLabel[] statsLabels;
 
     private int question;
     private int part;
 
+    private int[] stats;
+
     public CharacterCreationGUI()
     {
-        setSize(500, 700);
+        stats = new int[4];
+        for (int i = 0; i < stats.length; i++)
+            stats[i] = 0;
+
+        setSize(750, 300);
         setLayout(new BoxLayout(this.getContentPane(), BoxLayout.Y_AXIS));
 
-        questionLabel = new JLabel();
         questionPanel = new JPanel();
         questionPanel.setLayout(new FlowLayout());
+        questionLabel = new JLabel();
+        questionLabel.setFont(new Font("Courier New",Font.BOLD,18));
         questionPanel.add(questionLabel);
+
+        statsPanel = new JPanel();
+        statsPanel.setLayout(new FlowLayout());
+        statsLabels = new JLabel[4];
+        for (int i = 0; i < statsLabels.length; i++)
+        {
+            statsLabels[i] = new JLabel(STAT_NAMES[i] + ": 0 ");
+            statsLabels[i].setFont(new Font("Courier New",Font.PLAIN,16));
+            statsPanel.add(statsLabels[i]);
+        }
+
+
         answerPanel = new JPanel();
         answerPanel.setLayout(new FlowLayout());
 
         add(questionPanel);
+        add(statsPanel);
         add(answerPanel);
 
         question = 0;
         part = 0;
+
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
     }
 
+
+    /**
+     *  Changes the
+     */
     public void askNextQuestion()
     {
         answerPanel.removeAll();
@@ -80,11 +116,20 @@ public class CharacterCreationGUI extends JFrame implements ActionListener
             questions = STANDARD_QUESTIONS;
             answers = STANDARD_ANSWERS;
         }
-        else
+        else if(part == 1)
         {
             questions = FAVORITE_QUESTIONS;
             answers = FAVORITE_ANSWERS;
             prompt += "favorite ";
+        }
+        else
+        {
+            questionLabel.setFont(new Font("Courier New",Font.BOLD,18));
+            questionLabel.setText("Your character's stats:");
+            AnswerButton button =new AnswerButton("Start Game",-1);
+            button.addActionListener(this);
+            answerPanel.add(button);
+            return;
         }
         questionLabel.setText(prompt + questions[question] + "?");
 
@@ -105,6 +150,16 @@ public class CharacterCreationGUI extends JFrame implements ActionListener
     {
         if (!(e.getSource() instanceof AnswerButton))
             return;
+        AnswerButton button = (AnswerButton) e.getSource();
+
+        if(button.getID() == -1)
+        {
+            System.out.println("Starting the game!");
+            this.setVisible(false);
+            return;
+        }
+        if(question == FAVORITE_QUESTIONS.length-1)
+            startMusic(button.getText());
 
         String[] questions;
         HashMap<String, String[]> answers;
@@ -119,17 +174,19 @@ public class CharacterCreationGUI extends JFrame implements ActionListener
             answers = FAVORITE_ANSWERS;
         }
 
+        String[] values = answers.get(questions[question])[button.getID()].split(":")[1].split(","); //Get the stats values via: Get the possible answers array,get the answer associated with the button ID, get the values part of the answer, get the individual values
 
-        AnswerButton button = (AnswerButton) e.getSource();
-        String text = button.getText();
-        for (String s : answers.keySet())
-            System.out.println(s);
+        //Update stats
+        stats[0] += Integer.parseInt(values[0]);
+        stats[1] += Integer.parseInt(values[1]);
+        stats[2] += Integer.parseInt(values[2]);
+        stats[3] += Integer.parseInt(values[3]);
+        //Update Labels
+        statsLabels[0].setText(STAT_NAMES[0] + ": " + stats[0]);
+        statsLabels[1].setText(STAT_NAMES[1] + ": " + stats[1]);
+        statsLabels[2].setText(STAT_NAMES[2] + ": " + stats[2]);
+        statsLabels[3].setText(STAT_NAMES[3] + ": " + stats[3]);
 
-        String[] values = answers.get(questions[question])[button.getID()].split(":")[1].split(",");
-        System.out.println("Health increased by: " + values[0]);
-        System.out.println("Attack increased by: " + values[1]);
-        System.out.println("Defense increased by: " + values[2]);
-        System.out.println("Speed increased by: " + values[3]);
 
         if (question >= 2 && part == 0)
         {
@@ -138,11 +195,51 @@ public class CharacterCreationGUI extends JFrame implements ActionListener
         }
         else if (question >= 5 && part == 1)
         {
-            System.out.println("DONE!");
-            System.exit(0);
+            System.out.println("\nDONE!");
+            System.out.println("Final Stats: ");
+            System.out.println(STAT_NAMES[0] + ": " + stats[0] + " ");
+            System.out.println(STAT_NAMES[1] + ": " + stats[1] + " ");
+            System.out.println(STAT_NAMES[2] + ": " + stats[2] + " ");
+            System.out.println(STAT_NAMES[3] + ": " + stats[3] + " ");
+            for(JLabel l:statsLabels)
+                l.setFont(new Font("Courier New",Font.PLAIN,26));
+            part = -1;
+            question = -1;
         }
-
-        question++;
+        else
+            question++;
         askNextQuestion();
+    }
+
+    public static final String MUSIC_LOC = "music/";
+    public void startMusic(String type)
+    {
+        switch(type.toLowerCase())
+        {
+            case("jazz"):
+                playSound(MUSIC_LOC + "jazz1.wav");
+                break;
+            case("epic soundtracks"):
+                playSound(MUSIC_LOC + "epicsoundtracks1.wav");
+                break;
+            case("classical"):
+                playSound(MUSIC_LOC + "classical1.wav");
+                break;
+            case("club"):
+                playSound(MUSIC_LOC + "club1.wav");
+                break;
+
+
+        }
+    }
+
+    public static void playSound(String fileName)
+    {
+        System.out.println(fileName);
+        URL fileLoc = CharacterCreationGUI.class.getResource(fileName);
+        System.out.println(fileLoc);
+        System.out.println(fileLoc.getFile());
+        AudioClip clip = Applet.newAudioClip(fileLoc);
+        clip.play();
     }
 }
