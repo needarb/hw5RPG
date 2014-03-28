df7d7c042d494a047bb76db9dd2dcf3c6e4a7277
Ned Read <needarb@gmail.com>
2014-03-27 23:19:01 -0500

    Parts

--- src/edu/macalester/comp124/hw5/BlackKnight.java
+++ src/edu/macalester/comp124/hw5/BlackKnight.java
@@ -7,7 +7,16 @@ public class BlackKnight extends Enemy {
 
     public BlackKnight(String type, int speed, int health)
     {
-        super(type, speed, health, 5, 5);
+        super("blackKnight", speed, health, 5, 5);
+        defense = 4;
         imageFileName = "hw5RPG\\images\\monsters\\blackKnight.png";
+        AttackAction attack1 = new AttackAction(3,5,55,"Skull Bash");
+        AttackAction attack2 = new AttackAction(1,20,100,"Kick");
+        AttackAction attack3 = new AttackAction(2,10,90,"Slash");
+        combatActions.add(attack1);
+        combatActions.add(attack2);
+        combatActions.add(attack3);
     }
+
+
 }
---
+++ src/edu/macalester/comp124/hw5/music/BamRat.java
@@ -0,0 +1,14 @@
+package edu.macalester.comp124.hw5.music;
+
+import edu.macalester.comp124.hw5.Enemy;
+
+/**
+ * Created by needa_000 on 3/27/14.
+ */
+public class BamRat extends Enemy
+{
+    public BamRat(int speed, int health)
+    {
+        super("bamrat",speed,health,2,2);
+    }
+}
